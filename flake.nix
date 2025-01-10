816db1474cea0cabcb7600e011b4f1831576f473
Migelo <miha@filetki.si>
2025-01-10 13:37:49 +0000

    update nixpkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tree command, improved.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
